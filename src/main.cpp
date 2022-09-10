067f1d1f68c795404ce9f886bc1e8dbd783edf0c
dmitryivanov199 <dmitryivanov199@mail.ru>
2022-09-10 17:37:12 +0300

    Add pivot selecting

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+
 #include "quicksort.hpp"
 
 int main() {
+    srand(time(0));
     return 0;
 }
--- src/quicksort.cpp
+++ src/quicksort.cpp
@@ -1,8 +1,12 @@
+#include <iostream>
+
 #include "quicksort.hpp"
 
 static void make_quick_sorting(int *a, unsigned int n, unsigned int l, unsigned int r, pivot_index_type pivot_type);
 
 static bool is_base_case(unsigned int l, unsigned int r);
+
+static unsigned int choose_pivot_index(pivot_index_type pivot_type, unsigned int n);
 
 //----------------------------------------------------------------------------
 
@@ -16,8 +20,23 @@ void make_quick_sorting(int *a, unsigned int n, unsigned int l, unsigned int r, 
     if (is_base_case(l, r)) {
         return;
     }
+
+    unsigned int i{choose_pivot_index(pivot_type, n)};
 }
 
 bool is_base_case(unsigned int l, unsigned int r) {
     return l >= r;
+}
+
+unsigned int choose_pivot_index(pivot_index_type pivot_type, unsigned int n) {
+    switch (pivot_type) {
+    case pivot_index_type::FIRST:
+        return 0;
+
+    case pivot_index_type::LAST:
+        return n - 1;
+    
+    default:
+        return rand() % n;
+    }
 }
